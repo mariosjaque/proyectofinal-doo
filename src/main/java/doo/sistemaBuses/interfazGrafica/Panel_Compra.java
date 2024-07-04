5d65cb4fe7cb8f95c64fe5bfe86d41b33adfc788
Mario Andres Salgado Jaque <marisalgado2023@udec.cl>
2024-07-04 17:31:42 -0400

    cambiar tamaño de botones anterior/siguiente

--- src/main/java/doo/sistemaBuses/interfazGrafica/Panel_Compra.java
+++ src/main/java/doo/sistemaBuses/interfazGrafica/Panel_Compra.java
@@ -16,7 +16,7 @@ public class Panel_Compra extends JPanel {
 
         JButton retroceder = new JButton("Anterior");
         this.add(retroceder);
-        retroceder.setBounds(0,750,100,50);
+        retroceder.setBounds(0,750,150,50);
 
         ActionListener Retroceder = new ActionListener() {
             @Override

a724fb4402dcc68532869e8b0794f95127e2a15b
Santiago Alexander Diaz Barra <carldiaz2023@udec.cl>
2024-07-07 00:50:56 -0400

    Se agrega funcion para introducir datos de tarjeta y variables privadas que las contengan.

--- src/main/java/doo/sistemaBuses/interfazGrafica/Panel_Compra.java
+++ src/main/java/doo/sistemaBuses/interfazGrafica/Panel_Compra.java
@@ -8,9 +8,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Panel_Compra extends JPanel {
-
+    private JTextField textoTarjeta;
+    private JTextField textoCVV;
+    private JTextField textoNombre;
     public Panel_Compra(Panel_fondo fondo, Bus bus, ArrayList<Asiento> asientos){
         this.setLayout(null);
         this.setBounds(0,0,1000,1000);
@@ -31,22 +35,40 @@ public class Panel_Compra extends JPanel {
         ActionListener Credito = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                JLabel number_tarjeta = new JLabel("Ingresa número de tarjeta");
+                Panel_Compra.this.add(number_tarjeta);
+                number_tarjeta.setBounds(0,190,200,50);
+                JTextField texto_c = new JTextField();
+                Panel_Compra.this.add(texto_c);
+                texto_c.setBounds(0,200,200,50);
+                JLabel tarjeta_cvv = new JLabel("Ingresa CVV");
+                Panel_Compra.this.add(tarjeta_cvv);
+                JTextField texto_cvv = new JTextField();
+                Panel_Compra.this.add(texto_cvv);
+                texto_c.setBounds(0,300,200,50);
 
-                JTextField texto_c = new JTextField("Ingrese número de tarjeta");
-                texto_c.setBounds(0,100,200,50);
             }
+            //private void notify(String creditoSeleccionada) {
+            //}
         };
         ActionListener Debito = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                JTextField texto_d = new JTextField();
-                this.notify("Tarjeta de Debito seleccionada");
-                texto_d.setBounds(0,100,200,50);
+                JTextField texto_d = new JTextField("Ingrese número de tarjeta");
+                //this.notify("Tarjeta de Debito seleccionada");
+                Panel_Compra.this.add(texto_d);
+                texto_d.setBounds(0,200,250,50);
+                JLabel tarjeta_cvv = new JLabel("Ingresa CVV");
+                Panel_Compra.this.add(tarjeta_cvv);
+                JTextField texto_cvv = new JTextField();
+                Panel_Compra.this.add(texto_cvv);
+
             }
 
-            private void notify(String debitoSeleccionada) {
-            }
+            //private void notify(String debitoSeleccionada) {
+            //}
+
         };
         ActionListener Retroceder = new ActionListener() {
             @Override
@@ -67,4 +89,5 @@ public class Panel_Compra extends JPanel {
         g.setColor(Color.gray);
         g.fillRect(0,0,1000,1000);
     }
+
 }

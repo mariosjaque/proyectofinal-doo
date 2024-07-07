c9ed1b0a6ae2fcd9ebe59334ec0e8cb1b9e8c337
Santiago Alexander Diaz Barra <carldiaz2023@udec.cl>
2024-07-07 02:30:59 -0400

    Se añade generador pdf de pasajes para su obtencion inmediata.

---
+++ src/main/java/doo/sistemaBuses/interfazGrafica/Pasaje.java
@@ -0,0 +1,49 @@
+package doo.sistemaBuses.interfazGrafica;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Pasaje {
+    private Bus bus;
+    private int numeroAsiento;
+    private String nombrePasajero;
+    private String horarioFechaSalida;
+
+    public Pasaje(Bus bus, int numeroAsiento, String nombrePasajero, String horarioFechaSalida) {
+        this.bus = bus;
+        this.numeroAsiento = numeroAsiento;
+        this.nombrePasajero = nombrePasajero;
+        this.horarioFechaSalida = horarioFechaSalida;
+    }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public int getNumeroAsiento() {
+        return numeroAsiento;
+    }
+
+    public String getNombrePasajero() {
+        return nombrePasajero;
+    }
+
+    public String getHorarioFechaSalida() {
+        return horarioFechaSalida;
+    }
+
+    public void generarPDF(String filePath) throws FileNotFoundException, DocumentException {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream(filePath));
+        document.open();
+        document.add(new Paragraph("Información del Pasaje"));
+        document.add(new Paragraph("Bus: " + bus));
+        document.add(new Paragraph("Número de Asiento: " + numeroAsiento));
+        document.add(new Paragraph("Nombre del Pasajero: " + nombrePasajero));
+        document.add(new Paragraph("Horario y Fecha de Salida: " + horarioFechaSalida));
+        document.close();
+    }
+}
